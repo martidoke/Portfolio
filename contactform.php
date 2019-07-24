c8c5cb2c6912ca7a06704f5e39015437c1962d1d
Doke <Marti.Doke@informa.com>
2019-07-24 10:25:59 -0600

    trying_out_php.1

---
+++ contactform.php
@@ -0,0 +1,14 @@
+<?php
+
+if (isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+
+  $mailTo = "hello@martidoke.com";
+  $headers = "From: ".$email;
+  $txt = "You have received an email from ".$name.".\n\n".$message;
+
+  mail($mailTo, $txt, $headers);
+  header("Location: index.php?mailsend");
+}
